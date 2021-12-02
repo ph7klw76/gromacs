02bd9f33f6a0de0871fe0489f5667ddb68e0c712
Kai Lin Woon <55481379+ph7klw76@users.noreply.github.com>
2021-12-02 15:24:43 +0200

    Update curvefitting-type9-2.py
    updated with publication quality graph

--- curvefitting-type9-2.py
+++ curvefitting-type9-2.py
@@ -1,20 +1,13 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon May 17 11:03:04 2021
-Gromac torsional angle forecefield fitting-type 9
-@author: user
-"""
-
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 xdata=[]
 ydata=[]
-filepath='E:/mCBD-1.txt'
+filepath='D:/DBT1-1-2.txt'
 with open(filepath,'r') as f:
     for i,line in enumerate(f):
-        x=float(line.split()[0])*3.142/180
-        y=float(line.split()[1])*2600  # conversion au to KJ/mol
+        x=float(line.split()[0])
+        y=float(line.split()[1])*96.486  # conversion eV to KJ/mol
         xdata.append(x)
         ydata.append(y)
 xdata=np.asarray(xdata)
@@ -22,11 +15,11 @@ ydata=np.asarray(ydata)
 sigma=ydata*0.01+0.1 # weighted fitting
 def func(angle,k1,k2,k3,k4):  # defination of a function
     n1=2
-    n2=2
-    n3=4
-    n4=6
+    n2=1
+    n3=1
+    n4=0
     t1=0
-    t2=3.142
+    t2=3.142/2
     t3=0
     t4=0
     V=k1*(np.cos(n1*angle-t1)+1)+k2*(np.cos(n2*angle-t2)+1)+k3*(np.cos(n3*angle-t3)+1)+k4*(np.cos(n4*angle-t4)+1)
@@ -44,14 +37,20 @@ def drawgraph(func,x,y, p0):
         ii+=1   
     return popt, r2
 
-plt.scatter(xdata,ydata ,color='blue', label='QM simulation')       
-newx=np.linspace(0,3.142*2,100)
+fig, ax = plt.subplots(figsize=(12, 6))
+
+for label in (ax.get_xticklabels() + ax.get_yticklabels()):
+	label.set_fontsize(16)
+    
+plt.scatter(xdata,ydata ,color='blue', label='DFT/LC-*wpbeh/def2svp')       
+newx=np.linspace(-1,3.5,100)
 # plt.scatter(newx, func(newx, -1.239,1.243,2.741,0.4828),color='red',label='paper') 
 
 popt, r2=drawgraph(func,xdata,ydata, p0=[-1.239,1.243,2.741,0.4828])
 
-plt.scatter(newx, func(newx, *popt),color='orange',label='best fit') 
+plt.plot(newx, func(newx, *popt),color='orange',label='best fit') 
 
-plt.xlabel('dihedral angle (rad)')  
-plt.ylabel('KJ per mol')
-plt.legend()
+plt.xlabel('Torsional angle (rad)', fontsize=20)  
+plt.ylabel('KJ per mol', fontsize=20)
+plt.title('Torsional Potential of C37-N21-C22-C23',fontsize=20)
+plt.legend( prop={"size":20})
