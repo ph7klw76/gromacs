a0dec159b8cc50acdd0b5f69f09c21d9f69be614
Kai Lin Woon <55481379+ph7klw76@users.noreply.github.com>
2022-04-29 00:58:56 +0800

    Update buildP3HT.py

--- buildP3HT.py
+++ buildP3HT.py
@@ -1,29 +1,37 @@
-residue='0EKF'
+residue='57R4'
 ff=open('E:/test2.txt','w')
-for i in range(14):
-    myfile=open('E:/P3TT.txt','r')
+for i in range(2):
+    myfile=open('E:/P3TT3.txt','r')
  #   a=str(int(i+7))+residue
     deltaz=0.45*i
     for ii,line in enumerate(myfile):
         line=line.split()
-        a=int(line[0].split(residue)[0])+i*7
+        a=int(line[0].split(residue)[0])+i*224
+        print(a)
         a=str(a)+residue
-        b=line[1]
-        c=int(line[2])
+        if len(line)==5:
+            b=line[1]
+            b=b[:-5]
+            c=int(line[1].split(b)[1])
+            m=1
+        if len(line)==6: 
+            b=line[1]
+            c=int(line[2])
+            m=0
         if i>0:
-            c=1736*i+c
+            c=44352*i+c
             c=str(c)
-        d=float(line[3])
+        d=float(line[3-m])+4.7*i
         d=str('{0:.3f}'.format(d))
-        e=float(line[4])
+        e=float(line[4-m])
         e=str('{0:.3f}'.format(e))
-        f=float(line[5])
-        f=float(f)+0.65*i
+        f=float(line[5-m])# +0.345*i
+        f=float(f)
         f=str('{0:.3f}'.format(f))
         if int(c)<10000:
             txt='{:>9} {:>5} {:>4} {:>7} {:>7} {:>7}'.format(a,b,c,d,e,f)
         if int(c)>=10000:
-            c=b+c
+            c=str(b)+str(c)
             txt='{:>9} {:>10} {:>7} {:>7} {:>7}'.format(a,c,d,e,f)
         ff.write(txt+'\n')
     myfile.close()
