50d8ca94f116bcffdee0e8e89c54f8a87d645aa3
Kai Lin Woon <55481379+ph7klw76@users.noreply.github.com>
2022-05-06 12:20:33 +0800

    Create poly-poly-dis.py

---
+++ poly-poly-dis.py
@@ -0,0 +1,144 @@
+import numpy as np
+
+def finddistance(x1,y1,z1,x2,y2,z2):
+    x1,y1,z1,x2,y2,z2=float(x1),float(y1),float(z1),float(x2),float(y2),float(z2)
+    p1 = np.array([x1, y1, z1])
+    p2 = np.array([x2, y2, z2])
+    squared_dist = np.sum((p1-p2)**2, axis=0)
+    dist = np.sqrt(squared_dist)
+    return dist
+
+def extractxyz(line,residue):
+    line=line.lstrip()
+    line=line.split()
+    a=int(line[0].split(residue)[0])
+    if len(line)==8:
+        b=line[1]
+        b=b[:-5]
+        c=int(line[1].split(b)[1])
+        m=1
+    if len(line)==9: 
+        b=line[1]
+        c=int(line[2])
+        m=0
+    d=float(line[3-m])
+    e=float(line[4-m])
+    f=float(line[5-m])
+    return a,b,c,d,e,f
+
+def extract_relevant(file_w,file_r_itp,NTP,residue,itp1,itp2='None',itp3='None',itp4='None',itp5='None'): # match the aromatic ring
+    dopant_res=[]
+    file1=open(file_r_itp,'r') 
+    for i, dopant in enumerate(file1):
+        res0=dopant.split()[0]
+        res1=dopant.split()[1]
+        if res0==itp1:
+            dopant_res.append(res1)
+        if res0==itp2:
+            dopant_res.append(res1)
+        if res0==itp3:
+            dopant_res.append(res1)
+        if res0==itp4:
+            dopant_res.append(res1)
+        if res0==itp5:
+            dopant_res.append(res1)
+    file1.close()
+    file2=open(NPT,'r')
+    file3=open(file_w,'w')     
+    for line in file2:
+        line_s=line.split()
+        res_match=line_s[0][-4:]
+        if residue==res_match:
+            a,b,c,d,e,f=extractxyz(line,residue)
+            if b in dopant_res:
+                file3.write(line.lstrip())
+    file1.close()
+    file2.close()
+    file3.close()
+    return 
+
+def find_center(ff_dp,residue,ff1,number):
+    d1,e1,f1=[],[],[]
+    number=int(number)
+    ff=open(ff1,'w')
+    fff=open(ff_dp,'r')
+    for i,line in enumerate(fff): #6
+        if (i+1)%number!=0:
+            a,b,c,d,e,f=extractxyz(line,residue)
+            d1.append(float(d))
+            e1.append(float(e))
+            f1.append(float(f))
+        if (i+1)%number==0:
+            a,b,c,d,e,f=extractxyz(line,residue)
+            d1.append(float(d))
+            e1.append(float(e))
+            f1.append(float(f))
+            ff.write(str(a)+','+str(np.average(d1))+','+str(np.average(e1))+','+str(np.average(f1))+'\n')
+            d1,e1,f1=[],[],[]
+    ff.close()
+    fff.close()
+    return
+
+dopant_itp='E:/PHT/dopant.txt'
+polymer_itp='E:/PHT/polymer.txt'        
+residue_extract_1='P7F6'
+residue_extract_2='U9EL'  #
+ff_dopant='E:/PHT/'+residue_extract_1+'.txt'  #
+ff_polymer='E:/PHT/'+residue_extract_2+'.txt'  #
+ff_dopant2='E:/PHT/'+residue_extract_1+'_2.txt'  #
+ff_polymer2='E:/PHT/'+residue_extract_2+'_2.txt'  #
+NPT='E:/PHT/npt.gro'   #
+
+extract_relevant(ff_dopant,dopant_itp,NPT,residue_extract_1,'CAro')  #6
+extract_relevant(ff_polymer,polymer_itp,NPT,residue_extract_2,'CAro','S')  #5
+find_center(ff_dopant,residue_extract_1,ff_dopant2,6)
+find_center(ff_polymer,residue_extract_2,ff_polymer2,5)
+mydis,mya=[],[]
+myfile=open(ff_polymer,'r')
+
+
+file = open(ff_polymer2, 'r')
+line_count = 0
+for line in file:
+    if line != "\n":
+        line_count += 1
+file.close() 
+
+fw=open('E:/PHT/polymerminoror.txt','w')
+f=open(ff_polymer2,'r')
+lines=f.readlines()
+xyz_1,xyz_2,mydis,loc,pp,pp_loc=[],[],[],[],[],[]
+for i in range(line_count): #
+    xyz1=lines[i].split(',')
+    if (i+1)%10!=0:
+        xyz_1.append([xyz1[0],xyz1[1],xyz1[2],xyz1[3]])
+    if (i+1)%10==0:
+        xyz_1.append([xyz1[0],xyz1[1],xyz1[2],xyz1[3]])
+        for ii in range(line_count):
+            if (i!=ii and i!=ii+1 and i!=ii+2 and i!=ii+3 and i!=ii+4 and i!=ii+5 and i!=ii+6 and i!=ii+7 and i!=ii+8 and i!=ii+9): #
+                xyz2=lines[ii].split(',')
+                if (ii+1)%10!=0: #10 rings
+                    xyz_2.append([xyz2[0],xyz2[1],xyz2[2],xyz2[3]])
+                if (ii+1)%10==0:
+                    xyz_2.append([xyz2[0],xyz2[1],xyz2[2],xyz2[3]])
+                    for iii in range(len(xyz_1)):
+                        a1,x1,y1,z1= xyz_1[iii][0],xyz_1[iii][1],xyz_1[iii][2],xyz_1[iii][3]
+                        for iv in range(len(xyz_2)):
+                            a2,x2,y2,z2= xyz_2[iv][0],xyz_2[iv][1],xyz_2[iv][2],xyz_2[iv][3]
+                            dis=finddistance(x1,y1,z1,x2,y2,z2)
+                            mydis.append(dis) # distance between thiophene of different polymer
+                            loc.append([a1,a2])
+                    d=min(mydis) # minimum distance between 2 polymer
+                    dloc=loc[mydis.index(d)]
+                    print(dloc,d)
+                    pp.append(d)
+                    pp_loc.append(dloc)
+                    xyz_2,mydis,loc=[],[],[]
+        dd=min(pp) # minimum distance between 2 polymer
+        ddloc=pp_loc[pp.index(dd)]    
+        print("___________________________")
+        print(ddloc,dd)
+        # input()
+        fw.write(str(ddloc)+','+str(dd)+'\n')
+        xyz_1,pp_loc,pp=[],[],[]
+fw.close()
