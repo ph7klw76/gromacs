b432a0b9ab03348d415a8533eda275b5d5ee2aa6
Kai Lin Woon <55481379+ph7klw76@users.noreply.github.com>
2022-11-16 11:54:28 +0800

    Update sensordistance.py

--- sensordistance.py
+++ sensordistance.py
@@ -1,6 +1,7 @@
 import numpy as np
 resi='TTU7'
 myfile=open('F:/sensor2/out.gro','r') # change
+myfile=myfile.readlines()
 myfile2=open('F:/sensor2/NO.txt','r') # change
 myfile3=open('F:/sensor2/distance.txt','w') # change
 mydistance=[]
@@ -18,25 +19,25 @@ def finddistance(x1,y1,z1,x2,y2,z2):
     dist = np.sqrt(squared_dist)
     return dist
 
-for i,line in enumerate(myfile2):
+for i,line1 in enumerate(myfile2):
     n=i+1
-    line=line.split()
-    a1=line[0]
-    if len(line)==8:
-        b1=line[1]
+    line1=line1.split()
+    a1=line1[0]
+    if len(line1)==8:
+        b1=line1[1]
         b1=b1[:-5]
-        c1=int(line[1].split(b1)[1])
+        c1=int(line1[1].split(b1)[1])
         m1=1
-    if len(line)==9: 
-        b1=line[1]
-        c1=int(line[2])
+    if len(line1)==9: 
+        b1=line1[1]
+        c1=int(line1[2])
         m1=0
-    if len(line)==9 or len(line)==8:
+    if len(line1)==9 or len(line1)==8:
         a1=a1.split(resi)[0]
-        d1=float(line[3-m1])
-        e1=float(line[4-m1])
-        f1=float(line[5-m1])
-    for ii,line in enumerate(myfile):
+        d1=float(line1[3-m1])
+        e1=float(line1[4-m1])
+        f1=float(line1[5-m1])
+    for line in myfile:
         line=line.split()
         a=line[0]
         if len(line)==8:
@@ -56,11 +57,9 @@ for i,line in enumerate(myfile2):
         NOd=finddistance(d1,e1,f1,d,e,f)
         mydistance.append([NOd ,a1, a])
     if n%2==0:
-        mydistance=sorted(mydistance)[0]
-        print(mydistance)
-        myfile3.write(str(mydistance)+'\n')
+        mydistance1=sorted(mydistance)[0]
+        print(mydistance1)
+        myfile3.write(str(mydistance1)+'\n')
         mydistance=[]
-myfile.close()
 myfile2.close()
 myfile3.close()
-        
