ea667d6a610ed3e08e519dd18fc11827b5501130
Kai Lin Woon <55481379+ph7klw76@users.noreply.github.com>
2022-11-16 11:34:23 +0800

    Create sensordistance.py

---
+++ sensordistance.py
@@ -0,0 +1,66 @@
+import numpy as np
+resi='TTU7'
+myfile=open('F:/sensor2/out.gro','r') # change
+myfile2=open('F:/sensor2/NO.txt','r') # change
+myfile3=open('F:/sensor2/distance.txt','w') # change
+mydistance=[]
+
+def finddistance(x1,y1,z1,x2,y2,z2):
+    x1=float(x1)
+    y1=float(y1)
+    z1=float(z1)
+    x2=float(x2)
+    y2=float(y2)
+    z2=float(z2)
+    p1 = np.array([x1, y1, z1])
+    p2 = np.array([x2, y2, z2])
+    squared_dist = np.sum((p1-p2)**2, axis=0)
+    dist = np.sqrt(squared_dist)
+    return dist
+
+for i,line in enumerate(myfile2):
+    n=i+1
+    line=line.split()
+    a1=line[0]
+    if len(line)==8:
+        b1=line[1]
+        b1=b1[:-5]
+        c1=int(line[1].split(b1)[1])
+        m1=1
+    if len(line)==9: 
+        b1=line[1]
+        c1=int(line[2])
+        m1=0
+    if len(line)==9 or len(line)==8:
+        a1=a1.split(resi)[0]
+        d1=float(line[3-m1])
+        e1=float(line[4-m1])
+        f1=float(line[5-m1])
+    for ii,line in enumerate(myfile):
+        line=line.split()
+        a=line[0]
+        if len(line)==8:
+            b=line[1]
+            b=b[:-5]
+            c=int(line[1].split(b)[1])
+            m=1
+        if len(line)==9: 
+            b=line[1]
+            c=int(line[2])
+            m=0
+        if len(line)==9 or len(line)==8:
+            a=a.split(resi)[0]
+            d=float(line[3-m])
+            e=float(line[4-m])
+            f=float(line[5-m])
+        NOd=finddistance(d1,e1,f1,d,e,f)
+        mydistance.append([NOd ,a1, a])
+    if n%2==0:
+        mydistance=sorted(mydistance)[0]
+        print(mydistance)
+        myfile3.write(str(mydistance)+'\n')
+        mydistance=[]
+myfile.close()
+myfile2.close()
+myfile3.close()
+        
