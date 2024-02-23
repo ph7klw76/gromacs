bc71098605d4eebb6f91c86793d2e796e2ba1954
Kai Lin Woon <55481379+ph7klw76@users.noreply.github.com>
2024-02-23 23:21:40 +0800

    Update curvefitting-type9-2.py

--- curvefitting-type9-2.py
+++ curvefitting-type9-2.py
@@ -3,27 +3,19 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 xdata=[]
 ydata=[]
-filepath='D:/DBT1-1-2.txt'
+filepath='E:/DPPPz_scan.txt'
 with open(filepath,'r') as f:
     for i,line in enumerate(f):
-        x=float(line.split()[0])
-        y=float(line.split()[1])*96.486  # conversion eV to KJ/mol
+        x=float(line.split()[0])*np.pi/180 # convert to radiance
+        y=float(line.split()[1])*4.18  # conversion eV to KJ/mol
         xdata.append(x)
         ydata.append(y)
 xdata=np.asarray(xdata)
 ydata=np.asarray(ydata)
 sigma=ydata*0.01+0.1 # weighted fitting
-def func(angle,k1,k2,k3,k4):  # defination of a function
-    n1=2
-    n2=1
-    n3=1
-    n4=0
-    t1=0
-    t2=3.142/2
-    t3=0
-    t4=0
-    V=k1*(np.cos(n1*angle-t1)+1)+k2*(np.cos(n2*angle-t2)+1)+k3*(np.cos(n3*angle-t3)+1)+k4*(np.cos(n4*angle-t4)+1)
-    return V
+def rb_potential(phi, C0, C1, C2, C3, C4, C5, C6):
+    return (C0 + C1 * np.cos(phi) + C2 * np.cos(2*phi) + C3 * np.cos(3*phi) + 
+            C4 * np.cos(4*phi) + C5 * np.cos(5*phi) + C6 * np.cos(6*phi))
 
 def drawgraph(func,x,y, p0):
     popt, pcov = curve_fit(func, x, y,p0, sigma=sigma,absolute_sigma=True,maxfev= 100000)                                                             
@@ -43,12 +35,12 @@ for label in (ax.get_xticklabels() + ax.get_yticklabels()):
 	label.set_fontsize(16)
     
 plt.scatter(xdata,ydata ,color='blue', label='DFT/LC-*wpbeh/def2svp')       
-newx=np.linspace(-1,3.5,100)
+newx=np.linspace(0,2*np.pi,100)
 # plt.scatter(newx, func(newx, -1.239,1.243,2.741,0.4828),color='red',label='paper') 
 
-popt, r2=drawgraph(func,xdata,ydata, p0=[-1.239,1.243,2.741,0.4828])
+popt, r2=drawgraph(rb_potential,xdata,ydata, p0=[-1.239,1.243,2.741,0.4828,2.741,0.4828,0.4828]) # seven guess value
 
-plt.plot(newx, func(newx, *popt),color='orange',label='best fit') 
+plt.plot(newx, rb_potential(newx, *popt),color='orange',label='best fit') 
 
 plt.xlabel('Torsional angle (rad)', fontsize=20)  
 plt.ylabel('KJ per mol', fontsize=20)
